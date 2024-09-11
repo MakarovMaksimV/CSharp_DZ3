fb46e269e7408b7aeee36c96c2d0d3a5fa6de96d
Maksim <mirror0791@mail.ru>
2024-09-11 23:44:58 +0300

    Int

---
+++ DZ3/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections.Generic;
+using DZ3;
+
+namespace Seminar3;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Task3 task3 = new Task3();
+        task3.FindPath();
+        Console.ReadKey();
+    }
+}
---
+++ DZ3/Task3.cs
@@ -0,0 +1,88 @@
+﻿using System;
+namespace DZ3
+{
+	public class Task3
+	{
+        // Задан лабиринт в виде двумерного массива, где 1 - это стена, 0 - проход,
+        // 2 - выход, напишите алгоритм определяющий наличие выходов из лабиринта и
+        // координаты точки выхода, если такие имеются
+
+        public Stack<Tuple<int, int>> stack = new Stack<Tuple<int, int>>();
+        public int Count { get; set; }
+
+        public int[,] l = new int[,]{
+        {1, 1, 1, 2, 1, 1, 1 },
+        {1, 0, 0, 0, 0, 0, 1 },
+        {1, 0, 1, 1, 1, 0, 0 },
+        {0, 0, 0, 2, 1, 0, 2 },
+        {1, 1, 0, 0, 1, 1, 1 },
+        {1, 1, 1, 0, 1, 1, 1 },
+        {2, 1, 1, 2, 1, 1, 1 }};
+
+        public void FindPath()
+        {
+            Console.Write("Введите координату x: ");
+            int startI = Convert.ToInt32(Console.ReadLine());
+            Console.Write("Введите координату y: ");
+            int startJ = Convert.ToInt32(Console.ReadLine());
+            if (startI > l.GetLength(0)
+                || startJ > l.GetLength(1) || startI < 0 || startJ < 0)
+            {
+                Console.WriteLine("Вы ввели координаты за пределами лабиринта");
+                FindPath();
+            }
+            if (l[startI, startJ] == 1)
+            {
+                Console.WriteLine("Вы ввели координаты стены");
+                FindPath();
+            }
+            if (l[startI, startJ] == 2)
+            {
+                Console.WriteLine("Вы вышли из лабиринта");
+                FindPath();
+            }
+            else
+            {
+                HasExit(startI, startJ, l);
+            }
+        }
+
+        public void HasExit(int i, int j, int[,] l)
+        {
+            stack.Push(new Tuple<int, int>(i, j));
+
+            if (l[i, j] == 0)
+
+                while (stack.Count > 0)
+                {
+                    var current = stack.Pop();
+
+                    if (l[current.Item1, current.Item2] == 2 &&
+                            (current.Item1 + 1 >= l.GetLength(0)
+                            || current.Item2 + 1 >= l.GetLength(1)
+                            || current.Item1 - 1 < 0
+                            || current.Item2 - 1 < 0))
+                    {
+                        Count++;
+                    }
+
+                    l[current.Item1, current.Item2] = 1;
+
+                    if (current.Item1 + 1 < l.GetLength(0) && l[current.Item1 + 1, current.Item2] != 1)
+                        stack.Push(new(current.Item1 + 1, current.Item2));
+
+                    if (current.Item2 + 1 < l.GetLength(1) && l[current.Item1, current.Item2 + 1] != 1)
+                        stack.Push(new(current.Item1, current.Item2 + 1));
+
+                    if (current.Item1 > 0 && l[current.Item1 - 1, current.Item2] != 1)
+                        stack.Push(new(current.Item1 - 1, current.Item2));
+
+                    if (current.Item2 > 0 &&
+                        l[current.Item1, current.Item2 - 1] != 1)
+                        stack.Push(new(current.Item1, current.Item2 - 1));
+                }
+            Console.WriteLine($"Колличество выходов: {Count}");
+        }
+    }
+}
+
